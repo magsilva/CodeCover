441fc89f1d30d7d1f28785c3b9a13468c2099154
hanikesn <hanikesn@9ed44a15-1741-0410-93a4-efcaef9d7db3>
2013-02-22 13:43:04 +0000

    Added Pragma to enable/disable CodeCover completely
    git-svn-id: svn://svn.code.sf.net/p/codecover/code/trunk@150 9ed44a15-1741-0410-93a4-efcaef9d7db3

--- code/instrumentation-c/src/org/codecover/instrumentation/c/adapter/CCPreprocessor.java
+++ code/instrumentation-c/src/org/codecover/instrumentation/c/adapter/CCPreprocessor.java
@@ -1,12 +1,43 @@
 package org.codecover.instrumentation.c.adapter;
 
+import org.anarres.cpp.LexerException;
 import org.anarres.cpp.Preprocessor;
+import org.anarres.cpp.Token;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class CCPreprocessor extends Preprocessor {
+    private boolean ignoreTokens;
+
     public CCPreprocessor(File file) throws IOException {
         super(file);
+    }
+
+    @Override
+    protected void pragma(Token name, List<Token> value) throws IOException, LexerException {
+        if(!"CodeCover".equalsIgnoreCase(name.getText()))
+            return;
+
+        if (value.size() < 1)
+            return;
+
+        String cmd = value.get(1).getText();
+
+        if("On".equalsIgnoreCase(cmd)) {
+            ignoreTokens = false;
+        } else if("Off".equalsIgnoreCase(cmd)) {
+            ignoreTokens = true;
+        }
+    }
+
+    @Override
+    public Token token() throws IOException, LexerException {
+        Token t;
+        do {
+            t = super.token();
+        } while (ignoreTokens);
+        return t;
     }
 }
